4a4f660045fc0c35601c2c888c3c1be1143bf895
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2021-11-22 12:53:44 -0500

    commit

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < numberOfDrumButtons; i++) {	
 	document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-		// let audio = new Audio('sounds/tom-1.mp3');
-		// audio.play();
-
-		// this.style.color = "white";
-
+		
 		let buttonInnerHTML = this.innerHTML;
 		
 		switch(buttonInnerHTML) {
